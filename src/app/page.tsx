b351e83e02ec2308bf91c6d9f579430791939e6e
tallalmohar <tallalmoohar@gmail.com>
2025-06-02 02:01:04 -0700

    Big Gallary finished homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,16 @@ import { useState } from "react";
 		{
 			src: "/Homepage-Model3.jpg",
     	alt: "Model 3",
-    	caption: "Beautiful city view, with a Model Y standing in front",
+    	text: "Model 3",
+      apr: "0% APR Available",
+      link:"https://www.tesla.com/model3/design#overview"
 		},
 		{
 			src: "/Homepage-modely.webp",
     		alt: "Model y",
-    		caption: "Beautiful mountain view, with a Model Y standing in front",
+    		text: "Model Y",
+        apr: "1.99% APR Available",
+        link: "https://www.tesla.com/modely/design"
 		}
 	
 	]
@@ -32,28 +36,37 @@ export default function Home() {
   return (
     <>
     
-      <div className="flex flex-col w-full h-[716px]">
+      <div className="flex flex-col items-center w-full h-[716px]">
+        <h1 className="absolute pt-15 text-white text-6xl font-bold">{currentItem.text}</h1>
+        <h1 className="absolute pt-30 text-white text-xl underline ">{currentItem.apr}</h1>
+        <div className="absolute flex justify-centerw-[600px] flex-row z-10 gap-4">
+        <a  href={currentItem.link} className=" flex justify-center w-[170px] mt-40 p-4 bg-blue-600 text-white rounded hover:cursor-pointer hover:bg-blue-700 ">Order Now</a>
+        <a className=" flex justify-center w-[170px] mt-40 p-4  bg-white rounded hover:cursor-pointer hover:bg-gray-100  ">View Inventory</a>
+        </div>
         <img
         src={currentItem.src}
         alt={currentItem.alt}
         className="object-cover w-full h-full "  
       />
-      </div>
-      <div className="flex gap-4 mt-4">
+      <div className="absolute flex justify-around w-full mt-100">
+        
         <button
           onClick={goPrev}
-          className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded"
+          className="bg-gray-200 hover:bg-gray-300 hover:cursor-pointer px-4 py-2 rounded"
         >
-          Previous
+          <Image src="/Homepage - forward.svg" width={20} height={20} alt="forward"></Image>
         </button>
+        <button></button>
         <button
           onClick={goNext}
-          className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded"
+          className="bg-gray-200 hover:bg-gray-300  hover:cursor-pointer px-4 py-2 rounded"
         >
-          Next
+          <Image src="/Homepage- backward.svg" width={20} height={20} alt="forward"></Image>
         </button>
       </div>
    
+      </div>
+      
     </>
   );
 }
