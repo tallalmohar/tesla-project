2006302fe758b2e35b60dbae9140b1325246572c
tallalmohar <tallalmoohar@gmail.com>
2025-06-02 15:08:35 -0700

    updated scrollbar

--- src/app/components/ScrollCard.tsx
+++ src/app/components/ScrollCard.tsx
@@ -12,14 +12,15 @@ export default function ScrollCard({src, carType, carName, price}: ScrollCardPro
 
 	return (
 		<>
-				<div className="relative h-auto w-[1024px] flex-shrink-0">
+			
+				<div className="m-5 mt-0 mb-0 relative h-auto w-[1024px] flex-shrink-0">
 					<h1 className="absolute top-15 left-20 text-white text-md font-bold">{carType}</h1>
 					<h1 className="absolute top-100 left-20 text-white text-6xl font-bold">{carName}</h1>
 					<h1 className="absolute top-115 left-20 text-white text-md underline">{price}</h1>
 					<a  href={"#"} className="absolute top-85 left-20 flex justify-center w-[170px] mt-40 p-2 bg-blue-600 text-white rounded hover:cursor-pointer hover:bg-blue-700 ">Order Now</a>
         			<a className=" absolute top-85 left-70 flex justify-center w-[170px] mt-40 p-2  bg-white rounded hover:cursor-pointer hover:bg-gray-100  ">View Inventory</a>
 				
-				<img className="rounded-xl " src={src} width={1024} height={500} alt={"Different Tesla Cars"}></img>
+					<img className="rounded-xl " src={src} width={1024} height={500} alt={"Different Tesla Cars"}></img>
 				</div>
 			
 		</>
